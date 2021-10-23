b510d06fe2cb073fe3cba4e5391f1685836773eb
ericksilvadev <erick.silva6653@gmail.com>
2021-10-23 02:07:59 -0300

    fix login inputs

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,2 @@
 export { default as Button } from './Button';
-export { default as Inputs } from './Inputs';
 export { default as Onboarding } from './Onboarding';
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,18 @@
-import React, { useState, ChangeEventHandler } from 'react';
+import { useState, ChangeEventHandler } from 'react';
 import { Link } from 'react-router-dom';
+
 import { Button } from '../components';
 import logo from '../assets/images/logo.svg';
+import openEye from '../assets/images/password-visible.svg'
+import closedEye from '../assets/images/password-not-visible.svg'
 
 export default function Login() {
+  const [passwordVisibility, setVisibility] = useState(false);
   const [form, setForm] = useState({ email: '', password: '' });
+
+  const eyeIcon = passwordVisibility ? closedEye : openEye;
+  const isEmailEmpty = form.email ? 'not-empty' : '';
+  const isPassEmpty = form.password ? 'not-empty' : '';
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
     const { name, value } = target
@@ -25,22 +33,32 @@ export default function Login() {
             <h3>Fazer login</h3>
             <Link to="/sign-in">Criar uma conta</Link>
           </div>
-          <input
-            placeholder="E-mail"
-            type="text"
-            name="email"
-            className="first"
-            value={ form.email }
-            // onChange={ handleChange }
-          />
-          <input
-            placeholder="Senha"
-            type="password"
-            name="password"
-            className="second"
-            value={ form.password }
-            // onChange={ handleChange }
-          />
+          <div className="inputs">
+            <div className={`${isEmailEmpty} input-block`} data-placeholder="Email">
+              <input
+                type="text"
+                name="email"
+                className="first"
+                // value={ form.email }
+                // onChange={ handleChange }
+              />
+            </div>
+            <div className={`${isPassEmpty} input-block`} data-placeholder="Senha">
+              <input
+                type="password"
+                name="password"
+                className="second"
+                // value={ form.password }
+                // onChange={ handleChange }
+              />
+              <button className="show-password">
+                <img
+                  src={ eyeIcon }
+                  alt="mostrar/esconder senha"
+                />
+              </button>
+            </div>
+          </div>
           <div className="complement">
             <label htmlFor="remember">
               <input type="checkbox" id="remember" />
@@ -49,7 +67,7 @@ export default function Login() {
             <Link to="/recover-password">Esqueci minha senha</Link>
           </div>
           <Button
-            disabled={ true }
+            disabled={ false }
             name="Entrar"
             link="/main"
             color="green"
